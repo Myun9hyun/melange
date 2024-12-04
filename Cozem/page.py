fcf3af557ab6fc04b6c5111c8c19ef28c3664dd1
MyungHyun <126433920+Myun9hyun@users.noreply.github.com>
2024-12-04 13:01:28 +0900

    추가

--- Cozem/page.py
+++ Cozem/page.py
@@ -45,7 +45,9 @@ with st.sidebar:
     data = {
         'Name': ['💾Google Docs','📫문의방', 'Instagram'],
         'Link': ['[![GitHub](https://img.shields.io/badge/Excel-217346?style=for-the-badge&logo=microsoft-excel&logoColor=white)](https://onedrive.live.com/edit.aspx?resid=221CE48C87202DCA!2450&ithint=file%2cxlsx&authkey=!ADKQOeLCxzQp_5o)',
+         '[![GitHub](https://img.shields.io/badge/Kakao%20talk-FFBE00?style=for-the-badge&logo=kakaotalk&logoColor=white)](https://open.kakao.com/o/gUmZwuzd)',
          '[![GitHub](https://img.shields.io/badge/Kakao%20talk-FFBE00?style=for-the-badge&logo=kakaotalk&logoColor=white)](https://open.kakao.com/o/gUmZwuzd)']
+
     }
     df = pd.DataFrame(data)
     # st.sidebar.dataframe(df)
