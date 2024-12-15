ee2213aa8e1b8e9c8f9520ad527e0f4cd1bdaead
MyungHyun <126433920+Myun9hyun@users.noreply.github.com>
2024-12-15 17:31:38 +0900

    수정

--- Cozem/page.py
+++ Cozem/page.py
@@ -75,9 +75,12 @@ if choice == "메인페이지":
     * 경기 화성시 정남면 보통내길 205-28
     '''
     # 지도 위젯 추가
+
+
+    # 지도 좌표 설정
     location_data = {
-    'lat': [37.193674],  # 위도
-    'lon': [127.082698]  # 경도
+        'lat': [37.193674],  # 위도
+        'lon': [127.082698]  # 경도
     }
 
     # Pydeck 맵 설정
@@ -88,33 +91,25 @@ if choice == "메인페이지":
         pitch=0
     )
 
-    # 핀 모양 아이콘 설정
-    icon_data = [
-        {
-            'lat': 37.193674,  # 위도
-            'lon': 127.082698,  # 경도
-            'icon_data': {'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Map_pin_icon.svg/120px-Map_pin_icon.svg.png', 'width': 50, 'height': 50}
-        }
-    ]
-
     # 지도 스타일과 핀 모양 아이콘 표시
     deck = pdk.Deck(
-        map_style='mapbox://styles/mapbox/streets-v11',  # Mapbox 지도 스타일 사용
+        map_style='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',  # OpenStreetMap 사용
         initial_view_state=view_state,
         layers=[
             pdk.Layer(
-                "IconLayer",
-                icon_data,
+                'ScatterplotLayer',
+                data=location_data,
                 get_position='[lon, lat]',
-                get_icon='icon_data',
-                size_scale=15,  # 아이콘 크기 조정
-                pickable=True
+                get_radius=1000,  # 점의 크기
+                get_color='[255, 0, 0]',  # 빨간색으로 설정
+                pickable=True  # 마우스로 클릭할 수 있도록 설정
             )
         ]
     )
 
     # Streamlit에서 지도 표시
     st.pydeck_chart(deck)
+
 
 
 elif choice == "길드페이지":
